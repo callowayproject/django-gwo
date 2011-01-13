55f0093038292aecc624f18b4a9251e97f1dc1e1
Corey Oordt <coordt@washingtontimes.com>
2011-01-13 09:20:58 -0500

    Updated the example project to use the blog

--- example/settings.py
+++ example/settings.py
@@ -73,6 +73,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJ_ROOT, 'templates')
 )
 
 INSTALLED_APPS = (
@@ -81,6 +82,8 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.comments',
+    'blog',
     'gwo',
 )
 GWO_ACCOUNT = '1234'
