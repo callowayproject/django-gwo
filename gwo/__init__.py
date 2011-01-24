05eeb0f4f3f17e4ba8a381bdc7d09dac07609a1c
Corey Oordt <coordt@washingtontimes.com>
2011-01-24 08:55:13 -0500

    Version bump

--- gwo/__init__.py
+++ gwo/__init__.py
@@ -3,7 +3,7 @@ Django driven Google Website Optimizer testing
 """
 __version_info__ = {
     'major': 0,
-    'minor': 1,
+    'minor': 2,
     'micro': 0,
     'releaselevel': 'final',
     'serial': 1
